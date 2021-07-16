c889fe45e09df5db92c45e0b6bc6b73b9e7fd9af
Vinayak Jethe <jethevinayak@gmail.com>
2021-07-16 14:37:58 -0700

    update server 2

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ io.on('connection', socket => {
 })
 
 //server.listen(3000 || process.env.PORT)
-app.listen(process.env.PORT || 3000, function(){
+server.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
